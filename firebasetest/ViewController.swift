695e2d784d955e3c23373786ad8b401f10a1abe2
skrall3119 <alexjanci@yahoo.com>
2022-02-17 23:44:00 -0600

    Initial Commit

---
+++ firebasetest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebasetest
+//
+//  Created by Alex Janci on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
